7f2c23ef2da002d6094a13862bce84d479fef1e0
Syamsul MJ <syamsulmj94@gmail.com>
2020-02-08 03:19:29 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.868472a15ebe58796edeff621b2b229d.js"
+  "/portfolio/precache-manifest.1497af6ba05dff6a600945275aff64ab.js"
 );
 
 self.addEventListener('message', (event) => {
