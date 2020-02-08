be7a112c764ebdd69059aaa0901c6e043ab06fc0
Syamsul MJ <syamsulmj94@gmail.com>
2020-02-09 03:30:48 +0800

    Add experiences

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
 import { Avatar, Icon, Row, Col } from 'antd';
 import { getRealRoute } from '../../lib/Helpers';
 
 const Home = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [displaySkills, setDisplaySkills] = useState(false);
+
+  const getSkills = () => {
+    if (!isMobile || displaySkills) {
+      return null;
+    }
+    if (window.scrollY > 135 && !displaySkills) {
+      setDisplaySkills(true);
+      return null;
+    }
+    return "vanished";
+  }
+
+  useEffect(() => {
+    const onScroll = e => {
+      setScrollTop(e.target.documentElement.scrollTop);
+    };
+    
+    if (!displaySkills) {
+      window.addEventListener("scroll", onScroll);
+    }
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
   return (
     <React.Fragment>
       <div className="home">
@@ -44,41 +72,58 @@ const Home = () => {
               I have a vast knowledge in programming skills. My specialities field are more into web development
               and also have a pretty strong base in programming logics.
             </div>
+            <div className="title child">
+              <span>Experience</span>
+            </div>
+            <div className="description">
+              <ul>
+                <li>Fullstack Software Engineer at Naluri Hidup Sdn. Bhd.</li>
+                <li>Software Engineer (Software Graphics & Media Engineer) at Intel malaysia</li>
+                <li>Freelance Web Developer</li>
+                <li>Software Internship (web maintainer) in fave Malaysia</li>
+              </ul>
+              <div className="custom-button">
+                <Link to="/projects" className="btn">
+                  Get more details
+                  <Icon type="double-right"></Icon>
+                </Link>
+              </div>
+            </div>
           </Col>
-          <Col span={12} className="custom-col">
+          <Col span={12} className={`custom-col ${getSkills()}`}>
             <div className="title">
               <span>Technology used</span>
             </div>
             <div className="skills">
               <li>
-                <span class="skill-70"></span><em>Elixir</em>
+                <span className="skill-70"></span><em>Elixir</em>
               </li>
               <li>
-                <span class="skill-70"></span><em>Phoenix Framework</em>
+                <span className="skill-70"></span><em>Phoenix Framework</em>
               </li>
               <li>
-                <span class="skill-60"></span><em>PHP</em>
+                <span className="skill-60"></span><em>PHP</em>
               </li>
               <li>
-                <span class="skill-60"></span><em>Laravel</em>
+                <span className="skill-60"></span><em>Laravel</em>
               </li>
               <li>
-                <span class="skill-50"></span><em>Javascript</em>
+                <span className="skill-50"></span><em>Javascript</em>
               </li>
               <li>
-                <span class="skill-50"></span><em>React.JS</em>
+                <span className="skill-50"></span><em>React.JS</em>
               </li>
               <li>
-                <span class="skill-50"></span><em>Shell Script</em>
+                <span className="skill-50"></span><em>Shell Script</em>
               </li>
               <li>
-                <span class="skill-50"></span><em>Linux</em>
+                <span className="skill-50"></span><em>Linux</em>
               </li>
               <li>
-                <span class="skill-70"></span><em>HTML5</em>
+                <span className="skill-70"></span><em>HTML5</em>
               </li>
               <li>
-                <span class="skill-70"></span><em>CSS/SASS</em>
+                <span className="skill-70"></span><em>CSS/SASS</em>
               </li>
             </div>
           </Col>
