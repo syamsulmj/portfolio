204c9f484e62b7c14c4495ab41f20bec8b5a4638
Syamsul MJ <syamsulmj94@gmail.com>
2020-02-12 05:51:32 +0800

    Add projects page design

--- src/lib/Helpers.js
+++ src/lib/Helpers.js
@@ -1,9 +1,15 @@
 export const getRealPathname = () => {
-  const pathname = window.location.pathname;
-  const realPathname = pathname.includes("portfolio") ? pathname.replace("/portfolio/", "") : pathname.replace(/\//g, "");
+  const pathname = window.location.hash;
+  const realPathname = pathname.includes("portfolio") ? pathname.replace("/portfolio/", "") : pathname.replace("#/", "");
   return realPathname;
 }
 
 export const getRealRoute = () => {
   return window.location.pathname.replace(/\//g, "");
+}
+
+export const getImagePath = () => {
+  const pathname = window.location.pathname.replace(/\//g, "");;
+  const imagesPath = pathname === "portfolio" ? '/portfolio/images' : '/images';
+  return imagesPath;
 }
